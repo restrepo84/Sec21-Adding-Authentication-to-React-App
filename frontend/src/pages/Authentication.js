1287fad53e567aaf36b34bf5258fd9f4beff66e6
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-28 14:59:39 -0600

    attaching auth tokens to outgoing request

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,4 +1,5 @@
 import { json, redirect } from "react-router-dom";
+
 import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
@@ -11,7 +12,7 @@ export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
 
-  if (mode != "login" && mode !== "signup") {
+  if (mode !== "login" && mode !== "signup") {
     throw json({ message: "Unsupported mode." }, { status: 422 });
   }
 
@@ -20,6 +21,7 @@ export async function action({ request }) {
     email: data.get("email"),
     password: data.get("password"),
   };
+
   const response = await fetch("http://localhost:8080/" + mode, {
     method: "POST",
     headers: {
@@ -31,9 +33,15 @@ export async function action({ request }) {
   if (response.status === 422 || response.status === 401) {
     return response;
   }
+
   if (!response.ok) {
     throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
-  // soon: mange that token
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }
