dcbcadf5b0937fa98b7dd3b3c1ef847a0087c76b
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-28 17:02:30 -0600

    idk

--- backend/util/auth.js
+++ backend/util/auth.js
@@ -1,11 +1,11 @@
-const { sign, verify } = require('jsonwebtoken');
-const { compare } = require('bcryptjs');
-const { NotAuthError } = require('./errors');
+const { sign, verify } = require("jsonwebtoken");
+const { compare } = require("bcryptjs");
+const { NotAuthError } = require("./errors");
 
-const KEY = 'supersecret';
+const KEY = "supersecret";
 
 function createJSONToken(email) {
-  return sign({ email }, KEY, { expiresIn: '1h' });
+  return sign({ email }, KEY, { expiresIn: "1h" });
 }
 
 function validateJSONToken(token) {
@@ -17,26 +17,26 @@ function isValidPassword(password, storedPassword) {
 }
 
 function checkAuthMiddleware(req, res, next) {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
   if (!req.headers.authorization) {
-    console.log('NOT AUTH. AUTH HEADER MISSING.');
-    return next(new NotAuthError('Not authenticated.'));
+    console.log("NOT AUTH. AUTH HEADER MISSING.");
+    return next(new NotAuthError("Not authenticated."));
   }
-  const authFragments = req.headers.authorization.split(' ');
+  const authFragments = req.headers.authorization.split(" ");
 
   if (authFragments.length !== 2) {
-    console.log('NOT AUTH. AUTH HEADER INVALID.');
-    return next(new NotAuthError('Not authenticated.'));
+    console.log("NOT AUTH. AUTH HEADER INVALID.");
+    return next(new NotAuthError("Not authenticated."));
   }
   const authToken = authFragments[1];
   try {
     const validatedToken = validateJSONToken(authToken);
     req.token = validatedToken;
   } catch (error) {
-    console.log('NOT AUTH. TOKEN INVALID.');
-    return next(new NotAuthError('Not authenticated.'));
+    console.log("NOT AUTH. TOKEN INVALID.");
+    return next(new NotAuthError("Not authenticated."));
   }
   next();
 }
